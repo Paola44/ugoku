86cb46f0a4a3485625293960db243b12c51ed379
Julien Philippot <julien.philippot86@gmail.com>
2019-08-19 18:36:07 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Event.destroy_all
+User_event.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+
+nikol = User.create!(
+  name: 'Nikol',
+  email: 'nikol@example.fr',
+  password: 'password',
+  address: '135-127 Rue du Corps de Garde, 44100 Nantes',
+  photo:
+  favorite_sports:
+  )
+
+julien = User.create!(
+  name: 'Julien',
+  email: 'julien@example.fr',
+  password: 'password',
+  address: '21-1 Chemin de la Poignée, 44100 Nantes',
+  photo: ,
+  favorite_sports: ,
+  )
+
+richard = User.create!(
+  name: 'Richard',
+  email: 'richard@example.fr',
+  password: 'password',
+  address: '35 Boulevard Babin Chevaye, 44200 Nantes',
+  photo: ,
+  favorite_sports: ,
+  )
+
+richard = User.create!(
+  name: 'Richard',
+  email: Faker::Internet.free_email(name: user.name),
+  password: 'password',
+  address:
+  photo:
+  favorite_sports:
+  )
+puts 'Users created Yiha!!!'
