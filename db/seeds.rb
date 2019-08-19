4ffc308099b877c5a2f7be61a63a563b7dda8311
Julien Philippot <julien.philippot86@gmail.com>
2019-08-19 19:04:11 +0200

    Started seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,33 @@
+require 'open-uri'
+require 'nokogiri'
+
+puts 'Searching address...'
+
+url = 'https://hoodspot.fr/restaurant/nantes/'
+
+html_content = open('https://hoodspot.fr/restaurant/nantes/').read
+doc = Nokogiri::HTML(html_content)
+
+doc.search('uiSerpAddress uaLv3').each_with_index do |element, index|
+
+end
+
+
 puts 'Cleaning database...'
 
 Event.destroy_all
 User_event.destroy_all
 User.destroy_all
 
-puts 'Creating users...'
+puts 'Creating true users...'
 
 nikol = User.create!(
   name: 'Nikol',
   email: 'nikol@example.fr',
   password: 'password',
   address: '135-127 Rue du Corps de Garde, 44100 Nantes',
-  photo:
-  favorite_sports:
+  photo: File.open(Rails.root.join('db/fixtures/users/nikol.jpg')),
+  favorite_sports: ['crossfit', 'yoga'],
   )
 
 julien = User.create!(
@@ -20,8 +35,8 @@ julien = User.create!(
   email: 'julien@example.fr',
   password: 'password',
   address: '21-1 Chemin de la Poignée, 44100 Nantes',
-  photo: ,
-  favorite_sports: ,
+  photo: File.open(Rails.root.join('db/fixtures/users/julien.jpg')),
+  favorite_sports: ['crossfit', 'running', 'yoga'],
   )
 
 richard = User.create!(
@@ -29,11 +44,15 @@ richard = User.create!(
   email: 'richard@example.fr',
   password: 'password',
   address: '35 Boulevard Babin Chevaye, 44200 Nantes',
-  photo: ,
-  favorite_sports: ,
+  photo: File.open(Rails.root.join('db/fixtures/users/richard.jpg')),
+  favorite_sports: ['running', 'crossfit', 'foot'],
   )
 
-richard = User.create!(
+puts 'True users created Yiha!!!'
+puts 'Creating fake users...'
+
+
+user = User.create!(
   name: 'Richard',
   email: Faker::Internet.free_email(name: user.name),
   password: 'password',
@@ -41,4 +60,3 @@ richard = User.create!(
   photo:
   favorite_sports:
   )
-puts 'Users created Yiha!!!'
