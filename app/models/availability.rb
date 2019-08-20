5ec8660895b4c11856a9dbd920711c1c0a5edfb8
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-20 10:12:14 +0200

    models pour juju

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,4 @@
 class Availability < ApplicationRecord
   belongs_to :user
-  validates :day_name, presence: true, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday) }
+  validates :day_name, presence: true, inclusion: { in: %w(monday tuesday wednesday thursday friday saturday sunday) }
 end
