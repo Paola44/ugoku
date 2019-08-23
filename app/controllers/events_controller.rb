af1a75426adec80e63ca598406dc83767578d9d7
nikol lamilla <nikol.lamilla@gmail.com>
2019-08-23 13:27:36 +0200

    matching events + addpaths to availibilities

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,8 @@ class EventsController < ApplicationController
   def index
     @grouped_days_events = Event.all.order('start_at ASC').group_by { |event| event.start_at.to_date }
   end
+
+  def matching
+    @matching_events = current_user.user_events.where('matching')
+  end
 end
