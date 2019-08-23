bb583416f41e9f5f5312edc2a3bb34037a7f8e11
Julien Philippot <julien.philippot86@gmail.com>
2019-08-23 17:22:07 +0200

    fav sports

--- app/controllers/profile/favorite_sports_controller.rb
+++ app/controllers/profile/favorite_sports_controller.rb
@@ -3,5 +3,18 @@ module Profile
     def edit
       @user = current_user
     end
+
+    def update
+      @user = current_user
+      current_user.update(favorite_sport_params)
+
+      redirect_to dashboard_path
+    end
+
+    private
+
+    def favorite_sport_params
+      params.require(:user).permit(favorite_sports: [])
+    end
   end
 end
