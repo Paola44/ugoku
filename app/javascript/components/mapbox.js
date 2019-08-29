ce344642576109c923c2c6fc5bd0a1ffb8717e9b
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-29 12:42:17 +0200

    map resize

--- app/javascript/components/mapbox.js
+++ app/javascript/components/mapbox.js
@@ -9,20 +9,34 @@ if (mapElement) {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/kebseth/cjzuxfj070nbp1cphugepyazh'
+    style: 'mapbox://styles/kebseth/cjzuxfj070nbp1cphugepyazh',
+    width: 400,
+    height: 600
   });
+
   fontMaps.forEach((fontMap) => {
     fontMap.addEventListener('click', (event) => {
       mapElement.dataset.marker = fontMap.dataset.position
-      const marker = JSON.parse(mapElement.dataset.marker);
-      const coordinates = JSON.parse(fontMap.dataset.position)
+      const marker              = JSON.parse(mapElement.dataset.marker);
+      const coordinates         = JSON.parse(fontMap.dataset.position)
+
       new mapboxgl.Marker()
           .setLngLat([ marker[1], marker[0] ])
           .addTo(map)
+
       map.jumpTo({
-        center: [coordinates[1], coordinates[0]],
+        center: [marker[1], marker[0]],
         zoom: 15
       })
+      setTimeout(() => {
+        map.resize()
+        mapElement.style.opacity = 1
+      }, 200)
     })
   })
+
+  // $('#myModal').on('shown.bs.modal', function(event) {
+  //   console.log('yep')
+  //   map.resize()
+  // })
 }
