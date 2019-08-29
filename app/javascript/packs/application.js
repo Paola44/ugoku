777f4003f137767783df54d622eb06a40152ba2a
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-29 10:41:01 +0200

    card events + chevrons up down

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import '../components/carousel_dashboard';
 import '../components/checkbox-signin';
 import '../components/alert'
 import '../components/geolocalisedLoc'
-
+import '../components/chevrons';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import '../components/mapbox';
