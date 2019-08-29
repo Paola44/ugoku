e579ebbef05c06b37b39e8f63a2a8817adb9c885
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-29 14:53:26 +0200

    ajax matching

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,4 +39,38 @@ class EventsController < ApplicationController
       }
     end
   end
+
+  def create
+    @event = Event.find(params[:event_id])
+
+    user_event = UserEvent.new(event: @event, user: current_user)
+
+    if user_event.save
+      respond_to do |format|
+        format.html { redirect_to matching_events_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render '/events/matching' }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @event = UserEvent.find(params[:id]).event
+    @user_event = UserEvent.find(params[:id])
+    if @user_event.destroy
+      respond_to do |format|
+        format.html { redirect_to matching_events_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render '/events/matching' }
+        format.js
+      end
+    end
+  end
 end
